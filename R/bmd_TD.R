5ebcb2a165d01829833019920911331b12554d9f
angy89 <aserra@unisa.it>
2019-08-26 16:46:40 +0300

    prioritize prosition

--- R/bmd_TD.R
+++ R/bmd_TD.R
@@ -87,18 +87,21 @@ label2DMap = function(map, BMD, coord, myContour, th=0.95, mode = "mix", nDoseIn
     ttt_lab = 0 * ttt
     ttt_lab[ttt>0] = 1
   }else{
-    ttt_sum = ttt + ttt_area
+    mm = matrix(c(7,8,9,4,5,6,1,2,3),3,3) / 9
+    
+    ttt_sum = ttt + ttt_area + mm
     idx = which(ttt_sum == max(ttt_sum), arr.ind = T)
     ttt_lab = ttt_sum * 0
+    ttt_lab[idx[1,1],idx[1,2]] = 1
     
-    if(nrow(idx) == 1){
-      ttt_lab[idx[1,1],idx[1,2]] = 1
-    }else{
-      mm = matrix(c(7,8,9,4,5,6,1,2,3),3,3)
-      ttt_sum = ttt_sum + mm
-      idx = which(ttt_sum == max(ttt_sum), arr.ind = T)
-      ttt_lab[idx[1,1],idx[1,2]] = 1
-    }
+    # if(nrow(idx) == 1){
+    #   ttt_lab[idx[1,1],idx[1,2]] = 1
+    # }else{
+    #   mm = matrix(c(7,8,9,4,5,6,1,2,3),3,3) / 9
+    #   ttt_sum = ttt_sum + mm
+    #   idx = which(ttt_sum == max(ttt_sum), arr.ind = T)
+    #   ttt_lab[idx[1,1],idx[1,2]] = 1
+    # }
   }
   
   
