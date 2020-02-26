0f6de76eaa761089a30c2d1ef4fe9fc698e15849
angy89 <aserra@unisa.it>
2020-02-26 16:09:49 +0200

    added example script

---
+++ sample_data/set_parameters.R
@@ -0,0 +1,37 @@
+# parameters for phenodata
+dose_index = 2
+time_point_index = 3
+
+# #parameter for anova
+# pvalAnova = 0.01
+# pvalAnova.adj.method = "none"
+
+#parameter for fitting
+gridSize = 50
+pvalFitting = 0.01
+pvalFitting.adj.method = "fdr"
+modelSelection = 1:3
+logScale = TRUE
+
+# parameter BMD identification
+activity_threshold = 0.1
+BMD_response_threshold = 0.5
+mode = "most_left"
+timeLabels = c("Late","Middle","Early")
+doseLabels = c("Sensitive","Intermediate","Resilient")
+nTimeInt = 3
+nDoseInt = 3
+
+#parameter clusterings
+nClust = c(5,10,15,20,25)
+method="pearson"
+hls.method = "ward"
+
+#parameter enrichment
+corrType = "fdr"
+type_enrich="KEGG"
+org_enrich = "rnorvegicus"
+pth = 0.05
+sig = FALSE
+mis = 0
+only_annotated=FALSE
