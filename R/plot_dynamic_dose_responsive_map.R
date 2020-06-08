fb87ab58f3282d423c2e773c272f6dae5df5d19f
Angela Serra <aserra@unisa.it>
2020-06-08 18:34:29 +0300

    add plot dynamic_dose_responsive_map function

---
+++ R/plot_dynamic_dose_responsive_map.R
@@ -0,0 +1,22 @@
+#'
+#' This function takes in input the result of the function create_contour and plot the dynamic dose responsive activation map of a specific gene
+#'
+#' @param contour_res is the result object from the create_contour function
+#' @param geneName is the name of the gene
+#' @return a ggplot object
+#' @export
+#'
+plot_dynamic_dose_responsive_map = function(contour_res, geneName,activity_threshold,
+																						BMD_response_threshold,mode,nTimeInt,nDoseInt,
+																						timeLabels,doseLabels){
+
+	immy = contour_res$RPGenes[[geneName]][[3]]
+	coord = cbind(contour_res$RPGenes[[geneName]][[1]],contour_res$RPGenes[[geneName]][[2]])
+	res2 = compute_BMD_IC50(immy,coord, geneName,
+													activity_threshold = activity_threshold,
+													BMD_response_threshold = BMD_response_threshold,
+													mode = mode,
+													nTimeInt = nTimeInt,nDoseInt=nDoseInt,
+													timeLabels = timeLabels,
+													doseLabels = doseLabels,toPlot = T, addLegend = T)
+}
